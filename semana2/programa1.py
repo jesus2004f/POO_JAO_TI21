64df845fa459da9b453d6ec9eaa5613ea71eced1
YISUS <122810458+jesus2004f@users.noreply.github.com>
2023-01-23 17:01:46 +0000

    semana 2

---
+++ semana2/programa1.py
@@ -0,0 +1,9 @@
+"""  
+       Programa1
+       Nombre: Jesus AO
+       Fecha:23/01/2023
+       Descripcion: comentarios multilinea, comentarios de una linea y concatenacion de cadenas 
+"""    
+variable1 = "Hola"  # Variable que almacena una cadena de caracteres
+variable2 = "Python"  # variable que almacena una cadena de caracteres
+Print("variable1 + variable2) #concatena 2 cadenas"
